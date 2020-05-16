01964adba41a1b1297487eacf652230419b1366e
Milad E. Fahmy <miladezzat.f@gmail.com>
2020-05-16 12:55:53 +0200

    create store part 1

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
- function createStore(){
+ function createStore(reducer){
     // * 1-state tree
     // * 2- a way to get state
     // * 3- a way to listen changes of state
@@ -12,37 +12,15 @@
     const subscribe = (listener)=>{
         listeners.push(listener)
     }
-    
-    const dispatch = (action)=>{
-        state = reducer(state, action)
-        listeners.forEach(listener=>listener())
+
+    const dispatch =(action)=>{
+        state = reducer (state, action);
+        listeners.forEach(listener=> listener())
     }
+
     return{
         getState,
         subscribe, 
-        dispatch,       
+        dispatch           
     }
  }
-
-
- function todos = (state=[],action){
-     switch
- }
-
- let store = createStore()
-
- store.subscribe(()=>{
-     console.log("the state:", store.getState())
- })
- 
- store.dispatch(addtodo)
-
- const addtodo = {
-     type: "ADD_TODO",
-     todo:{
-         id:1,
-         name: "Eating",
-         complete: false,
-         state: null,
-     }
- } 
